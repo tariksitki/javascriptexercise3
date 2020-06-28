86d016fab705f475d85d64022c13bc96dd5108b1
tariksitki <tariksitki0119@gmail.com>
2020-06-28 22:33:22 +0200

    JavaScript 86 87

--- main.js
+++ main.js
@@ -63,3 +63,67 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
       console.log(alternate_Sums([1, 3, 6, 2, 5, 10]))
 
 
+
+
+
+      // Exercise 86   
+
+
+
+
+
+      function angle_Type(angle) {
+        if(angle < 90) {
+          return "Acute angle.";
+        }
+        if(angle === 90) {
+          return "Right angle.";
+        }
+        if(angle < 180) {
+          return "Obtuse angle.";
+        }
+        return "Straight angle.";
+      }
+      
+      console.log(angle_Type(47))
+      console.log(angle_Type(90))
+      console.log(angle_Type(145))
+      console.log(angle_Type(180))
+
+
+
+
+
+      // Exercise 87   
+
+
+
+      function array_checking(arra1, arra2) {
+
+        for(var i = 0; i < arra1.length; i++) {
+          for(var j = i; j < arra1.length; j++) {
+            var result = true,
+              temp = arra1[i];
+            arra1[i] = arra1[j];
+            arra1[j] = temp;
+            for(var k = 0; k < arra1.length; k++) {
+              if(arra1[k] !== arra2[k]) {
+                result = false;
+                break;
+              }
+            }
+            if(result) {
+              return true;
+            }
+            arra1[j] = arra1[i];
+            arra1[i] = temp;
+          }
+        }
+        return false;
+      }
+      
+      console.log(array_checking([10,20,30], [10,20,30]))
+      console.log(array_checking([10,20,30], [30,10,20]))
+      console.log(array_checking([10,20,30,40], [10,30,20,40]))
+
+
