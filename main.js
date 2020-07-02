cfc2cfad8c659cbe1a100c06e69c0eaff68a2358
tariksitki <tariksitki0119@gmail.com>
2020-07-02 23:05:08 +0200

    Js 93

--- main.js
+++ main.js
@@ -258,26 +258,46 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
 
 
 
-      //  Exercise 93
+      //  Exercise 93    Verilen degerler arasindaki mataématiksel olarak en yüksek deger farkini bulma
+      
 
 
-      function array_max_diff(arr) {
+        function array_max_diff(arr) {
+          
+          var max_result = 0;
 
-        var max_result = 0;
-    
-        for(var i=0;i<arr.length;i++)
-           {
-            for(var k=0; k!=i && k<arr.length; k++)
-            {
-                var diff = Math.abs(arr[i]-arr[k]);
+          for (var i = 0; i<arr.length; i++) {
+            
+            for (var k=0; k!=i && k<arr.length; k++)
+
+            {   var diff = Math.abs(arr[i] - arr[k]) ;
+
                 max_result = Math.max(max_result, diff);
-            }
+
+
+
+             }
+            
+          }
+                return max_result;
         }
-        return max_result;
-    }
-    console.log(array_max_diff([1, 2, 3, 8, 9]))
-    console.log(array_max_diff([1, 2, 3, 18, 9]))
-    console.log(array_max_diff([13, 2, 3, 8, 9]))
+
+          console.log(array_max_diff([1,2,3,8,9]));
+
+          console.log(array_max_diff([1,2,3,18,9]));
+
+          console.log(array_max_diff([13,2,3,8,9]));
+          
+          
+          
+
+
+
+
+
+
+
+
 
 
 
