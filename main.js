f3cac467be2950c10e8188304e3ba447d9177136
tariksitki <tariksitki0119@gmail.com>
2020-07-02 22:54:35 +0200

    JavaScript 92 93

--- main.js
+++ main.js
@@ -238,4 +238,52 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
 
 
 
+      // Exercıse 92 
+
+      function max_difference(arr) {
+        var max = -1;
+          var temp;
+        for (var i = 0; i < arr.length - 1; i++)
+            {
+          temp = Math.abs(arr[i] - arr[i + 1]);
+          max = Math.max(max, temp);
+          }
+        return max;
+      }
+      
+      console.log(max_difference([1, 2, 3, 8, 9]))
+      console.log(max_difference([1, 2, 3, 18, 9]))
+      console.log(max_difference([13, 2, 3, 8, 9]))
+
+
+
+
+      //  Exercise 93
+
+
+      function array_max_diff(arr) {
+
+        var max_result = 0;
+    
+        for(var i=0;i<arr.length;i++)
+           {
+            for(var k=0; k!=i && k<arr.length; k++)
+            {
+                var diff = Math.abs(arr[i]-arr[k]);
+                max_result = Math.max(max_result, diff);
+            }
+        }
+        return max_result;
+    }
+    console.log(array_max_diff([1, 2, 3, 8, 9]))
+    console.log(array_max_diff([1, 2, 3, 18, 9]))
+    console.log(array_max_diff([13, 2, 3, 8, 9]))
+
+
+
+
+
+
+
+
 
