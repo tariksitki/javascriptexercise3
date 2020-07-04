b127839b40ec558fa996f2c346b2f715a4dbda4a
tariksitki <tariksitki0119@gmail.com>
2020-07-04 22:09:22 +0200

    JavaScript 98

--- main.js
+++ main.js
@@ -369,15 +369,40 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
 
 
 
+
+
+              // Exercise 98   Switch case of the minimum possible number of letters to make a given string written in the upper case or in the lower case
               
-
-              
-          
-          
-          
+              // Kücük yada Büyük harfle yazili olarak Verilen string i  en az harfi degistirerek ya hepsini büyük yada hepsini kücük yapmak
 
 
 
+
+
+
+                      function change_case(new_str) {
+
+                        var x = 0;
+    
+                        var y = 0;
+    
+                        for (var i = 0; i< new_str.length; i++) { 
+    
+                          if(/[A-Z]/.test(new_str[i])) {
+    
+                            x++;
+                          }  
+                              else y++ ;
+                        }
+                        
+                            if (y > x) return new_str.toLowerCase();
+    
+                            return new_str.toUpperCase();
+                      }
+    
+                          console.log(change_case("Write"));
+    
+                          console.log(change_case("PHp"));
 
 
 
