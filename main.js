23e2bacec3ae43edaf232fe9d52356839efa2a88
tariksitki <tariksitki0119@gmail.com>
2020-07-07 23:59:23 +0200

    Java Script 100

--- main.js
+++ main.js
@@ -441,5 +441,19 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
 
 
 
+           // Exercise     100 
 
+
+
+
+           function check_common_element(arra1, arra2) {
+            for (var i = 0; i < arra1.length; i++)
+            {
+              if (arra2.indexOf(arra1[i]) != -1) 
+                return true;
+            }
+            return false;
+          }
+          console.log(check_common_element([1,2,3], [3,4,5]));   
+          console.log(check_common_element([1,2,3], [5,6,7]));   
 
