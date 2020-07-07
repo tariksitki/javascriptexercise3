76111666e6c1bbbb87aac187713999be4f143d76
tariksitki <tariksitki0119@gmail.com>
2020-07-07 23:58:20 +0200

    JavaScript 99

--- main.js
+++ main.js
@@ -409,6 +409,32 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
 
 
 
+                          // Exercıse 99
+
+
+
+
+                          function rearrangement_characters(str1, str2) {
+
+                            var first_set = str1.split(''),
+                                second_set = str2.split(''),
+                                result = true;
+                          
+                            first_set.sort();
+                            second_set.sort();
+                          
+                            for (var i = 0; i < Math.max(first_set.length, second_set.length); i++) {
+                              if (first_set[i] !== second_set[i]) {
+                                result = false;
+                              }
+                            }
+                          
+                            return result;
+                          }
+                          
+                          console.log(rearrangement_characters("xyz", "zyx"))
+                          console.log(rearrangement_characters("xyz", "zyp"))
+
 
 
 
