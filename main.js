4ee392f9b7518380f080aae9a2ea07a9f9162121
tariksitki <tariksitki0119@gmail.com>
2020-07-10 23:35:34 +0200

    Js 114

--- main.js
+++ main.js
@@ -581,6 +581,27 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
 
 
 
+                    // Exercise 114  
+
+
+                    function is_correct_Sentence(input_str) {
+                      var first_char = input_str[0];
+                      var last_char = input_str[input_str.length - 1];
+                      return /[A-Z]/.test(first_char) && last_char == "."
+                  }
+                  console.log(is_correct_Sentence("This tool will help you write better English and efficiently corrects texts."));
+                  console.log(is_correct_Sentence("This tool will help you write better English and efficiently corrects texts"));            
+                  console.log(is_correct_Sentence("this tool will help you write better English and efficiently corrects texts."));
+
+
+
+
+                    // Exercise 115 
+
+
+
+
+
                     
                         
 
