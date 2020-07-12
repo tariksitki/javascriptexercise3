b10ba8d095b1233f07168f4b1c8f84f486bf11e3
tariksitki <tariksitki0119@gmail.com>
2020-07-12 23:51:50 +0200

    Js 126

--- main.js
+++ main.js
@@ -689,6 +689,24 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
 
 
 
+
+
+                  // Exercise 126
+
+
+                  function max_even(arra) {
+
+                    arra.sort((x, y) => y - x);
+                  
+                    for (var i = 0; i < arra.length; i++) {
+                      if (arra[i] % 2 == 0)
+                        return arra[i];
+                      }
+                    }
+                  
+                  console.log(max_even([20, 40, 200]));
+                  console.log(max_even([20, 40, 200, 301]));
+
                         
 
 
