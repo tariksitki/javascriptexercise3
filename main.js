05da0addf898ce83ef05b1e80193ec7a4118c4c4
tariksitki <tariksitki0119@gmail.com>
2020-07-13 22:57:41 +0200

    Js 129

--- main.js
+++ main.js
@@ -741,7 +741,29 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
                 console.log(nearest_round_number(56));
                 console.log(nearest_round_number(592));
 
-                
+
+
+
+                //   Exercise 129
+
+
+                function next_Prime_num(num) {
+                  for (var i = num + 1;; i++) {
+                      var isPrime = true;
+                      for (var d = 2; d * d <= i; d++) {
+                          if (i % d === 0) {
+                              isPrime = false;
+                              break;
+                          }
+                      }
+                      if (isPrime) {
+                          return i;
+                      }
+                  }
+              }
+              
+              console.log(next_Prime_num(3));
+              console.log(next_Prime_num(17));
 
 
                         
