75f12fa88f43dbfd8e467f444bb94c3bf798bdec
tariksitki <tariksitki0119@gmail.com>
2020-07-14 23:30:45 +0200

    JavaScript 130

--- main.js
+++ main.js
@@ -766,6 +766,25 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
               console.log(next_Prime_num(17));
 
 
+
+              // Exercise 130
+
+
+              
+              function even_digits(num) {
+                var ctr = 0;
+                while (num) {
+                  ctr += num % 2 === 0;
+                  num = Math.floor(num / 10);
+                }
+                return ctr;
+              }
+              
+              console.log(even_digits(123));
+              console.log(even_digits(1020));
+              console.log(even_digits(102));
+
+
                         
 
 
