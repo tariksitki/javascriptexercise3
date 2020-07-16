b85fba776eae9dc3a19d4fad46a86d9ab881ddfa
tariksitki <tariksitki0119@gmail.com>
2020-07-16 23:26:42 +0200

    JavaScript 135

--- main.js
+++ main.js
@@ -881,6 +881,31 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
               console.log(change_char("abcxyz"));
               console.log(change_char("python"));  
 
+
+
+
+
+              //  Exercise 135
+
+
+              function remove_duplicate_cchars(str) {
+                var arr_char = str.split("");
+                var result_arr = [];
+              
+                for (var i = 0; i < arr_char.length; i++) {
+                  if (str.indexOf(arr_char[i]) === str.lastIndexOf(arr_char[i]))
+                    result_arr.push(arr_char[i]);
+                  }
+              
+                return result_arr.join("");
+              }
+              console.log(remove_duplicate_cchars("abcdabc"));
+              console.log(remove_duplicate_cchars("python"));
+              console.log(remove_duplicate_cchars("abcabc"));
+              console.log(remove_duplicate_cchars("1365451"));
+
+              
+
                         
 
 
