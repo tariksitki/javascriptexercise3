badefbcf5242cc5c045689d1dab66a834999797b
tariksitki <tariksitki0119@gmail.com>
2020-07-17 23:46:18 +0200

    Js 137

--- main.js
+++ main.js
@@ -923,6 +923,29 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
 
 
 
+
+
+
+                // Exercise 137
+
+
+                function test_fifteen(num) 
+                {
+                    while(num<15){
+                        num++;
+                    }
+                    return num;
+                }
+                console.log(test_fifteen("123"));
+                console.log(test_fifteen("10"));
+                console.log(test_fifteen("5"));
+
+
+
+
+                
+
+
               
 
 
