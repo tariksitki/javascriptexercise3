143e76b1e491de1fa000b5eec64e9de56f37eb7b
tariksitki <tariksitki0119@gmail.com>
2020-07-18 22:43:14 +0200

    JavaScript 139

--- main.js
+++ main.js
@@ -960,6 +960,32 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
                 console.log(sixteen_bits_reverse(5));
 
 
+
+
+
+
+                // Exercise 139
+
+
+
+                function find_rightmost_round_number(input_arr) {
+
+                  var result = 0;
+                  for (var i = 0; i < input_arr.length; i++)
+                  {
+                    if (input_arr[i] % 10 === 0) {
+                      result = i;
+                    }
+                  }
+                
+                  return result;
+                }
+                
+                console.log(find_rightmost_round_number([1, 22, 30, 54, 56]));
+                console.log(find_rightmost_round_number([1, 22, 32, 54, 56]));
+                console.log(find_rightmost_round_number([1, 22, 32, 54, 50]));
+
+
               
 
 
