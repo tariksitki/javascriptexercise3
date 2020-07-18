131043f74fb6ac129b379d740979113fc765a944
tariksitki <tariksitki0119@gmail.com>
2020-07-18 22:44:13 +0200

    JavaScript 140

--- main.js
+++ main.js
@@ -986,6 +986,25 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
                 console.log(find_rightmost_round_number([1, 22, 32, 54, 50]));
 
 
+
+
+
+                // Exercise 140 
+
+
+
+                function test_same_digit(num) {
+                  var first = num % 10;
+                  while (num) {
+                    if (num % 10 !== first) return false;
+                num = Math.floor(num / 10);
+                  }
+                  return true
+                }
+                
+                console.log(test_same_digit(1234));
+                console.log(test_same_digit(1111));
+                console.log(test_same_digit(22222222));
               
 
 
