051d7cc4efc2ad845d790f6d1a64f1c2aa501d1d
tariksitki <tariksitki0119@gmail.com>
2020-07-21 22:52:08 +0200

    JavaScript 144

--- main.js
+++ main.js
@@ -1088,5 +1088,28 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
                   console.log(sort_by_string_length(["xyz","acd","aa","bb","zzz","", "a","b"]));
                     
                     
+
+
+
+                  // Exercise 144 
+
+
+                  function break_address(url_add) {
+                    var data = url_add.split("://")
+                    var protocol = data[0];
+                    data = data[1].split(".com");
+                    var domain = data[0];
+                    data = data[1].split("/");
+                
+                    if(data[1]){
+                        return [protocol,domain,data[1]]
+                    }
+                
+                    return [protocol,domain]
+                }
+                
+                var url_add = "https://www.w3resource.com/javascript-exercises/"
+                console.log("Original address: "+url_add)
+                console.log(break_address(url_add))
                     
 
