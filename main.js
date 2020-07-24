213cfc759d81bfa2501a721ec6e434abeecce2cb
tariksitki <tariksitki0119@gmail.com>
2020-07-24 22:18:02 +0200

    JavaScript 147

--- main.js
+++ main.js
@@ -1169,3 +1169,22 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
                 
                 console.log(sum_Of_Cubes(3));
                 console.log(sum_Of_Cubes(4));
+
+
+
+                // Exercise 147
+
+
+                function sum_digits_from_string(dstr) {
+                  var dsum = 0;
+                
+                  for (var i = 0; i < dstr.length; i++)
+                  {
+                
+                    if (/[0-9]/.test(dstr[i])) dsum += parseInt(dstr[i])
+                  }
+                  return dsum;
+                }
+                
+                console.log(sum_digits_from_string("abcd12efg9"))
+                console.log(sum_digits_from_string("w3resource"))
