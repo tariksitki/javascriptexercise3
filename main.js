62e354fb2212af8bce212ad79244f6168b0e4531
tariksitki <tariksitki0119@gmail.com>
2020-08-02 21:15:45 +0200

    Js 1

--- main.js
+++ main.js
@@ -1262,3 +1262,47 @@ console.log(longest_string(['a', 'aa', 'aaa', 'aaaa' ]));
 console.log(swap_adjacent_digits(1234));
 console.log(swap_adjacent_digits(123456));
 console.log(swap_adjacent_digits(12345));
+
+
+
+
+
+
+    // Exercise 1
+
+    var today = new Date();
+  var day = today.getDay();
+  var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+  console.log("Today is : " + daylist[day] + ".");
+  var hour = today.getHours();
+  var minute = today.getMinutes();
+  var second = today.getSeconds();
+  var prepand = (hour >= 12)? " PM ":" AM ";
+  hour = (hour >= 12)? hour - 12: hour;
+  if (hour===0 && prepand===' PM ') 
+  { 
+  if (minute===0 && second===0)
+  { 
+  hour=12;
+  prepand=' Noon';
+  } 
+  else
+  { 
+  hour=12;
+  prepand=' PM';
+  } 
+  } 
+  if (hour===0 && prepand===' AM ') 
+  { 
+  if (minute===0 && second===0)
+  { 
+  hour=12;
+  prepand=' Midnight';
+  } 
+  else
+  { 
+  hour=12;
+  prepand=' AM';
+  } 
+  } 
+console.log("Current Time : "+hour + prepand + " : " + minute + " : " + second);
